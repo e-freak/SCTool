de3a3b1781b61dec8c484d9d5f7d6b2a1ef85386
ishihara <Rieko.Ishihara@jrits.ricoh.co.jp>
2017-05-17 10:41:42 +0900

    Hello, world

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * index.js
+ * 
+ * @author yuki
+ */
+
+'use strict';
+
+const electron = require('electron');
+
+
+
+let mainWindow;
+
+electron.app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') {
+        electron.app.quit();
+    }
+});
+
+electron.app.on('ready', () => {
+    mainWindow = new electron.BrowserWindow({ width: 1024, height: 768 });
+    mainWindow.loadURL(`file://${__dirname}/app/view/title.html`);
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
