0b95538b12ec5acb29293aa79b1ec322e258b98d
Rieko Ishihara <Rieko.Ishihara@jrits.ricoh.co.jp>
2017-05-26 15:56:13 +0900

    メイン画面実装

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ electron.app.on('window-all-closed', () => {
 });
 
 electron.app.on('ready', () => {
-    mainWindow = new electron.BrowserWindow({ width: 1024, height: 768 });
-    mainWindow.loadURL(`file://${__dirname}/app/view/title.html`);
+    mainWindow = new electron.BrowserWindow({ 
+                                                            width: 1024, 
+                                                            height: 768,
+                                                            useContentsSize: true });
+    mainWindow.loadURL(`file://${__dirname}/app/view/main.html`);
     mainWindow.on('closed', () => {
         mainWindow = null;
     });
