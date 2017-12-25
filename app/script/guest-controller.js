1e49d701201e445ce5d4f330ccc686d0aeed872a
ishihara <Rieko.Ishihara@jrits.ricoh.co.jp>
2017-12-25 14:34:43 +0900

    update guest menu

--- app/script/guest-controller.js
+++ app/script/guest-controller.js
@@ -16,6 +16,10 @@ var _createClass = (function () { function defineProperties(target, props) { for
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var GuestController = (function () {
+
+    // TODO:　削除ボタンによって、旧アイコンが変更になってしまう
+    //        javasctipt側でテーブルを準備して、対応させること
+
     function GuestController(view) {
         _classCallCheck(this, GuestController);
 
@@ -28,34 +32,41 @@ var GuestController = (function () {
         key: 'initialize',
         value: function initialize() {
             //alert('GuestController::initialize');
-
             this._view.getElementById('guest-form-add-button').addEventListener('click', this.onClickAddButton.bind(this));
         }
     }, {
         key: 'onClickAddButton',
         value: function onClickAddButton() {
-            //alert('GuestController::onClickAddButton()');
 
             var name = this._view.getElementById('guest-form-name').value;
             var age = this._view.getElementById('guest-form-age').value;
 
-            if (name) {
-                var table = this._view.getElementById('guest-list-table');
-                var row = table.insertRow(-1);
-                var cell_guest_info_icon = row.insertCell(-1);
-                var cell_guest_info_name = row.insertCell(-1);
-                var cell_guest_info_age = row.insertCell(-1);
-                var cell_guest_delete_button = row.insertCell(-1);
-
-                cell_guest_info_name.innerHTML = name;
-                cell_guest_info_age.innerHTML = age;
-
-                // 追加されたボタンにIDとイベントリスナーを追加
-                var id = 'guest-delete-button-' + row.rowIndex;
-                cell_guest_delete_button.innerHTML = '<input type="button" id="' + id + '" class="guest-delete-button" value="-">';
-
-                this._view.getElementById(id).addEventListener('click', this.onClickDeleteButton.bind(this, id));
+            if (!(name && age)) {
+                return;
             }
+
+            // 行を追加する
+            var table = this._view.getElementById('guest-list-table');
+            var row = table.insertRow(-1);
+            var cell_guest_info_avatar = row.insertCell(-1);
+            var cell_guest_info_name = row.insertCell(-1);
+            var cell_guest_info_age = row.insertCell(-1);
+            var cell_guest_delete_button = row.insertCell(-1);
+
+            // 名前と年齢はそのまま表示
+            cell_guest_info_name.innerHTML = name;
+            cell_guest_info_age.innerHTML = age;
+
+            // アバターは行に応じたIDを割り振り、画像は年齢に応じたものにする
+            var srcFilePath = this._selectAvatar(age);
+
+            var avatarID = 'guest-avatar-' + row.rowIndex;
+            cell_guest_info_avatar.innerHTML = '<img id="' + avatarID + '" class="guest-avatar" src="' + srcFilePath + '" alt="Avatar">';
+
+            // ボタンも行に応じたIDを割り振り、行削除用のイベントリスナーを追加する
+            var buttonID = 'guest-delete-button-' + row.rowIndex;
+            cell_guest_delete_button.innerHTML = '<input type="button" id="' + buttonID + '" class="guest-delete-button" value="-">';
+            this._view.getElementById(buttonID).addEventListener('click', this.onClickDeleteButton.bind(this, buttonID));
         }
     }, {
         key: 'onClickDeleteButton',
@@ -63,30 +74,75 @@ var GuestController = (function () {
 
             // TODO:中間行をダブルクリックしているとまとめて行が消えてしまう問題へ対応
 
-            var button = this._view.getElementById(buttonID);
+            var targetButton = this._view.getElementById(buttonID);
+            var table = targetButton.parentNode.parentNode.parentNode;
+            var rowsLength = table.rows.length;
+            var targetRow = targetButton.parentNode.parentNode;
 
-            var table = button.parentNode.parentNode.parentNode;
-            var oldRowsLength = table.rows.length;
+            var deleted = false;
 
-            // 全ボタンのイベントリスナーを削除する
-            for (var i = 0; i < oldRowsLength; ++i) {
-                var oldID = 'guest-delete-button-' + i;
-                this._view.getElementById(oldID).removeEventListener('click', this.onClickDeleteButton);
+            for (var i = 0; i < rowsLength; ++i) {
+
+                // 対象行は削除して終了する
+                if (i === targetRow.rowIndex) {
+                    table.deleteRow(i);
+                    deleted = true;
+
+                    // テーブルが空になったら、テーブルごと削除してしまう
+                    if (!table.rows.length) {
+                        table.parentNode.removeChild(table);
+                    }
+
+                    continue;
+                }
+
+                // まだ削除が実行されていない場合はそのままの状態を維持する
+                if (!deleted) {
+                    continue;
+                }
+
+                // 以後はdeleted === trueの削除行の処理
+
+                var newIndex = i - 1;
+
+                var oldAvaterID = 'guest-avatar-' + i;
+                var newAvaterID = 'guest-avatar-' + index;
+                var oldAvaterElement = this._view.getElementById(oldAvaterID);
+
+                var oldButtonID = 'guest-delete-button-' + i;
+                var newButtonID = 'guest-delete-button-' + index;
+                var oldButtonElement = this._view.getElementById(oldButtonID);
+
+                // ボタンの削除イベントは古いIDに紐付いているのでbindし直す
+                oldButtonElement.removeEventListener('click', this.onClickDeleteButton);
+                oldButtonElement.addEventListener('click', this.onClickDeleteButton.bind(this, newButtonID));
+
+                // IDを新しい行に合わせて更新する
+                oldAvaterElement.id = newAvaterID;
+                oldButtonElement.id = newButtonID;
+            }
+        }
+    }, {
+        key: '_selectAvatar',
+        value: function _selectAvatar(age) {
+
+            var fileName_prefix_gender = Math.random() >= 0.5 ? 'man-' : 'woman-';
+
+            var fileName_age = '';
+
+            if (age >= 0 && age <= 15) {
+                fileName_age = 'child1.png';
+            } else if (age > 15 && age <= 40) {
+                fileName_age = 'young1.png';
+            } else if (age > 40 && age <= 65) {
+                fileName_age = 'middle-aged1.png';
+            } else if (age > 65) {
+                fileName_age = 'elder1.png';
+            } else if (age < 0 || age > 99 || age === undefined) {
+                return;
             }
 
-            // 対象行を削除
-            var row = button.parentNode.parentNode;
-            table.deleteRow(row.rowIndex);
-
-            var newRowsLength = table.rows.length;
-
-            // 残ったボタンに新規IDとイベントリスナーを振り直す
-            for (var j = 0; j < newRowsLength; ++j) {
-                var newID = 'guest-delete-button-' + j;
-                var cell_guest_delete_button = table.rows[j].cells[3];
-                cell_guest_delete_button.innerHTML = '<input type="button" id="' + newID + '" class="guest-delete-button" value="-">';
-                cell_guest_delete_button.addEventListener('click', this.onClickDeleteButton.bind(this, newID));
-            }
+            return '../image/avatar/' + fileName_prefix_gender + fileName_age;
         }
     }]);
 
--- src/main/js/guest-controller.js
+++ src/main/js/guest-controller.js
@@ -9,6 +9,9 @@
 
 export default class GuestController {
 
+    // TODO:　削除ボタンによって、旧アイコンが変更になってしまう
+    //        javasctipt側でテーブルを準備して、対応させること
+
     constructor(view) {
         //alert('GuestController::constructor()');
 
@@ -17,70 +20,117 @@ export default class GuestController {
 
     initialize() {
         //alert('GuestController::initialize');
-
         this._view.getElementById('guest-form-add-button').addEventListener('click', this.onClickAddButton.bind(this));
-
     }
 
     onClickAddButton() {
-        //alert('GuestController::onClickAddButton()');
 
         const name = this._view.getElementById('guest-form-name').value;
         const age = this._view.getElementById('guest-form-age').value;
 
-        if (name) {
-            let table = this._view.getElementById('guest-list-table');
-            let row = table.insertRow(-1);
-            let cell_guest_info_icon = row.insertCell(-1);
-            let cell_guest_info_name = row.insertCell(-1);
-            let cell_guest_info_age = row.insertCell(-1);
-            let cell_guest_delete_button = row.insertCell(-1);
-
-            cell_guest_info_name.innerHTML = name;
-            cell_guest_info_age.innerHTML = age;
-
-            // 追加されたボタンにIDとイベントリスナーを追加
-            const id = `guest-delete-button-${row.rowIndex}`;
-            cell_guest_delete_button.innerHTML = `<input type="button" id="${id}" class="guest-delete-button" value="-">`;
-
-            this._view.getElementById(id).addEventListener('click', this.onClickDeleteButton.bind(this, id));
+        if (!(name && age)) {
+            return;
         }
 
+        // 行を追加する
+        let table = this._view.getElementById('guest-list-table');
+        let row = table.insertRow(-1);
+        let cell_guest_info_avatar = row.insertCell(-1);
+        let cell_guest_info_name = row.insertCell(-1);
+        let cell_guest_info_age = row.insertCell(-1);
+        let cell_guest_delete_button = row.insertCell(-1);
+
+        // 名前と年齢はそのまま表示
+        cell_guest_info_name.innerHTML = name;
+        cell_guest_info_age.innerHTML = age;
+
+        // アバターは行に応じたIDを割り振り、画像は年齢に応じたものにする
+        const srcFilePath = this._selectAvatar(age);
+
+        const avatarID = `guest-avatar-${row.rowIndex}`;
+        cell_guest_info_avatar.innerHTML = `<img id="${avatarID}" class="guest-avatar" src="${srcFilePath}" alt="Avatar">`;
+
+        // ボタンも行に応じたIDを割り振り、行削除用のイベントリスナーを追加する
+        const buttonID = `guest-delete-button-${row.rowIndex}`;
+        cell_guest_delete_button.innerHTML = `<input type="button" id="${buttonID}" class="guest-delete-button" value="-">`;
+        this._view.getElementById(buttonID).addEventListener('click', this.onClickDeleteButton.bind(this, buttonID));
     }
 
     onClickDeleteButton(buttonID) {
 
         // TODO:中間行をダブルクリックしているとまとめて行が消えてしまう問題へ対応
 
+        const targetButton = this._view.getElementById(buttonID);
+        const table = targetButton.parentNode.parentNode.parentNode;
+        const rowsLength = table.rows.length;
+        const targetRow = targetButton.parentNode.parentNode;
 
+        let deleted = false;
 
-        const button = this._view.getElementById(buttonID);
+        for (let i = 0; i < rowsLength; ++i) {
 
-        const table = button.parentNode.parentNode.parentNode;
-        const oldRowsLength = table.rows.length;
+            // 対象行は削除して終了する
+            if (i === targetRow.rowIndex) {
+                table.deleteRow(i);
+                deleted = true;
 
-        // 全ボタンのイベントリスナーを削除する
-        for (let i = 0; i < oldRowsLength; ++i) {
-            const oldID = `guest-delete-button-${i}`;
-            this._view.getElementById(oldID).removeEventListener('click', this.onClickDeleteButton);
+                // 削除後、行が空になったらテーブルの存在ごと削除してしまう
+                // 背景色などの無駄なCSS効果が残るのを防ぐ
+                if (!table.rows.length) {
+                    table.parentNode.removeChild(table);
+                }
+
+                continue;
+            }
+
+            // まだ削除が実行されていない場合はそのままの状態を維持する
+            if (!deleted) {
+                continue;
+            }
+
+            // 削除した後の行情報を更新する
+
+            const newIndex = i - 1;
+
+            const oldAvaterID = `guest-avatar-${i}`;
+            const newAvaterID = `guest-avatar-${index}`;
+            const oldAvaterElement = this._view.getElementById(oldAvaterID);
+
+            const oldButtonID = `guest-delete-button-${i}`;
+            const newButtonID = `guest-delete-button-${index}`;
+            const oldButtonElement = this._view.getElementById(oldButtonID);
+
+            // ボタンの削除イベントは古いIDに紐付いているのでbindし直す
+            oldButtonElement.removeEventListener('click', this.onClickDeleteButton);
+            oldButtonElement.addEventListener('click', this.onClickDeleteButton.bind(this, newButtonID));
+
+            // IDを新しい行に合わせて更新する
+            oldAvaterElement.id = newAvaterID;
+            oldButtonElement.id = newButtonID;
         }
 
-        // 対象行を削除
-        const row = button.parentNode.parentNode;
-        table.deleteRow(row.rowIndex);
-
-        const newRowsLength = table.rows.length;
-
-        // 残ったボタンに新規IDとイベントリスナーを振り直す
-        for (let j = 0; j < newRowsLength; ++j) {
-            const newID = `guest-delete-button-${j}`;
-            let cell_guest_delete_button = table.rows[j].cells[3];
-            cell_guest_delete_button.innerHTML = `<input type="button" id="${newID}" class="guest-delete-button" value="-">`;
-            cell_guest_delete_button.addEventListener('click', this.onClickDeleteButton.bind(this, newID));
-        }
     }
 
+    _selectAvatar(age) {
 
+        let fileName_prefix_gender = (Math.random() >= 0.5) ? 'man-' : 'woman-';
 
+        let fileName_age = '';
+
+        if (age >= 0 && age <= 15) {
+            fileName_age = 'child1.png';
+        } else if (age > 15 && age <= 40) {
+            fileName_age = 'young1.png';
+        } else if (age > 40 && age <= 65) {
+            fileName_age = 'middle-aged1.png';
+        } else if (age > 65) {
+            fileName_age = 'elder1.png';
+        } else if (age < 0 || age > 99 || age === undefined) {
+            return;
+        }
+
+        return '../image/avatar/' + fileName_prefix_gender + fileName_age;
+
+    }
 
 }
